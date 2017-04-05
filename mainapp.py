828c7a9ca77232d7a6e8459fa7c27f79c72bcaff
bs2kbs2k <bs2k@naver.com>
2017-04-05 18:39:45 +0900

    CCI&pygame add

--- mainapp.py
+++ mainapp.py
@@ -1,1 +1,12 @@
-import guibox
+import sys
+import pygame
+from pygame.locals import *
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((640, 480))
+pygame.display.set_caption('Hello, World!')
+while True:
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+	pygame.display.update()
