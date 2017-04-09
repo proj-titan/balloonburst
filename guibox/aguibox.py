241f155e2a2cf8156f177ffc6026f4a7368c206a
bs2kbs2k <bs2k@naver.com>
2017-04-09 10:59:29 +0900

    All Done!!!

--- guibox/aguibox.py
+++ guibox/aguibox.py
@@ -2,16 +2,21 @@ import pygame
 from pygame.locals import *
 class balloon():
     """docstring for balloon."""
-    def __init__(self, transform):
+    def __init__(self, transform, img):
         self.transform = transform
-        self.obj = \
-            pygame.Rect(transform[0],transform[1], 10,10)
+        self.obj = img
+        self.collider = pygame.Rect\
+        ( transform[0]
+        , transform[1]
+        , 154
+        , 200)
     #
     def draw(self, SURF):
-        pygame.draw.ellipse(SURF,
-            (255, 255, 255),
-            self.obj, 5)
+        #pygame.draw.ellipse(SURF,
+        #    (255, 255, 255),
+        #    self.obj, 5)
+        SURF.blit(self.obj, self.transform)
     #
     def setTransform(self, arg):
-        self.obj.topleft = arg
         self.transform = arg
+        self.collider.topleft = arg
--- mainapp.py
+++ mainapp.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import time
 from random import *
 from pygame.locals import *
 import guibox.aguibox as GUI
@@ -7,25 +8,46 @@ pygame.init()
 DISPLAYX, DISPLAYY = 640, 480
 DISPLAYSURF = pygame.display.set_mode((DISPLAYX, DISPLAYY))
 balloonList = []
+mpos = (0, 0)
 pygame.display.set_caption('Burst Balloon!')
+BLIST = [ pygame.image.load('balloon.png')
+        , pygame.image.load('balloon_green.png')]
+gpoint = 0
+FONTOBJ = pygame.font.Font('freesansbold.ttf', 32)
 def randomCreate():
-    x = randint(10, DISPLAYX - 10)
+    x = randint(10, DISPLAYX - 164)
     y = DISPLAYY - 1
-    return GUI.balloon((x, y))
+    c = randint(0, 1)
+    return GUI.balloon((x, y), BLIST[c])
 def update():
-	global balloonList
-	DISPLAYSURF.fill((0, 0, 0))
-	for looper in range(randint(1, 3)):
-		balloonList.append(randomCreate())
-	for looper in balloonList:
-		newTransform = list(looper.transform)
-		newTransform[1] += 1 # NOTE: don't del this!
-		looper.setTransform(newTransform)
-		looper.draw(DISPLAYSURF)
+    global balloonList, gpoint
+    DISPLAYSURF.fill((255, 255, 255))
+    if randint(1,100) == 55:
+        for looper in range(randint(0, 3)):
+            balloonList.append(randomCreate())
+    for looper in balloonList:
+        newTransform = list(looper.transform)
+        newTransform[1] -= 1 # NOTE: don't del this!
+        looper.setTransform(newTransform)
+        #looper.draw(DISPLAYSURF)
+        DISPLAYSURF.blit(looper.obj, looper.transform)
+    textsurf = FONTOBJ.render(str(gpoint), True, (0,0,0),(255,255,255))
+    textrect = textsurf.get_rect()
+    textrect.topleft = (10,10)
+    DISPLAYSURF.blit(textsurf, textrect)
+    for index, val in enumerate(balloonList):
+        if val.collider.collidepoint(mpos):
+            del balloonList[index]
+            gpoint += 1
+        if val.collider.y < 0:
+            del balloonList[index]
+
 while True:
-	update()
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			pygame.quit()
-			sys.exit()
-	pygame.display.update()
+    update()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == MOUSEMOTION:
+            mpos = event.pos
+    pygame.display.update()
